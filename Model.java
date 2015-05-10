105a16a026dff3e9fd4c77d573218e51d875f789
Cannonbait <ivar_busig@hotmail.com>
2015-05-10 15:30:28 +0200

    Added detection and handling of collisions with other balls

--- Model.java
+++ Model.java
@@ -14,6 +14,7 @@ public class Model implements IBouncingBallsModel {
 		this.areaWidth = width;
 		this.areaHeight = height;
 		balls.add(new Ball(1, 5));
+    balls.add(new Ball(1, 2));
 	}
 
 	@Override
@@ -24,8 +25,16 @@ public class Model implements IBouncingBallsModel {
     applyGravity(deltaT);
 
     moveBalls(deltaT);
+
+    clearCollisions();
 		
 	}
+
+  private void clearCollisions(){
+    for (Ball ball : balls){
+      ball.emptyCollisions();
+    }
+  }
 
   private void handleCollisions(double deltaT){
     ballCollisions(deltaT);
@@ -36,7 +45,9 @@ public class Model implements IBouncingBallsModel {
   private void ballCollisions(double deltaT){
     for (Ball a : balls){
       for (Ball b : balls){
-        if (a != b && a.collidesWith(b)){
+        if (a != b && a.collidesWith(b) && a.hasNotCollidedWith(b)){
+          a.addCollision(b);
+          b.addCollision(a);
           ballCollision(a, b);
         }
       }
@@ -44,13 +55,40 @@ public class Model implements IBouncingBallsModel {
   }
 
   private void ballCollision(Ball a, Ball b){
+    Vector xNorm = new Vector(1, 0), yNorm = new Vector(0, 1);
     Vector collisionVector = calculateCollisionVector(a, b);
+
+    Vector aV = a.getMovementVector();
+    Vector aP = aV.projection(collisionVector);
+    Vector aR = aV.rejection(collisionVector);
+
+    Vector bV = b.getMovementVector();
+    Vector bP = bV.projection(collisionVector);
+    Vector bR = bV.rejection(collisionVector);
+
+    //Based on aP and bP we need to calculate the new vectors on the collision axis
+    //final double momentum 
+    
+    Vector newAp = bP;
+    Vector newAVectorVx = newAp.projection(xNorm).add(aR.projection(xNorm));
+    Vector newAVectorVy = newAp.projection(yNorm).add(aR.projection(yNorm));
+    System.out.println("From: " + a.getVy() + "\t To: " + newAVectorVy.getY());
+    a.setVx(newAVectorVx.getX());
+    a.setVy(newAVectorVy.getY());
+    
+
+    Vector newBp = aP;
+    Vector newBVectorVx = newBp.projection(xNorm).add(bR.projection(xNorm));
+    Vector newBVectorVy = newBp.projection(yNorm).add(bR.projection(yNorm));
+    System.out.println("From: " + b.getVy() + "\t To: " + newBVectorVy.getY());
+    b.setVx(newBVectorVx.getX());
+    b.setVy(newBVectorVy.getY());
   }
 
   private Vector calculateCollisionVector(Ball a, Ball b){
     double x = a.getX() - b.getX();
     double y = a.getY() - b.getY();
-    return (new Vector(x, y)).normalize();
+    return new Vector(x, y).normalize();
   }
 
   private void wallCollisions(double deltaT){
